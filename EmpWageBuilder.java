43ec7565ca5b8740d5cdd21a5bf869e58e6b19dd
Swapnil_Kamble <swapnilskamble010@gmail.com>
2022-03-10 17:11:24 +0530

    UC2 logic is added

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -5,12 +5,22 @@ public class EmpWageBuilder {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program");
 		int IS_FULL_TIME = 1;
+		int EMP_RATE_PER_HOUR = 20;
+		int empHours = 0;
+		int empWage = 0;
+		
 		double empCheck = Math.floor(Math.random() * 10) % 2;
 		
-		if (empCheck == IS_FULL_TIME)
+		if (empCheck == IS_FULL_TIME) {
 			System.out.println("Employee is Present");
-		else
+			empHours = 8;
+			empWage = empHours * EMP_RATE_PER_HOUR;
+			System.out.println("Employee Wage: " + empWage);
+		}	
+		else {
 			System.out.println("Employee is Absent");
-	}
+			System.out.println("Employee Wage: " + empWage);
+		}
+	}		
 
 }
