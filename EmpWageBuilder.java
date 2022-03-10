c47b1bc882136e2baf485ac152c242d47ded3779
Swapnil_Kamble <swapnilskamble010@gmail.com>
2022-03-10 21:21:07 +0530

    UC 4 switch case logic implemented

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -4,27 +4,31 @@ public class EmpWageBuilder {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program");
-		int IS_FULL_TIME = 1;
-		int IS_PART_TIME = 2;
-		int EMP_RATE_PER_HOUR = 20;
+		final int IS_FULL_TIME = 1;
+		final int IS_PART_TIME = 2;
+		final int EMP_RATE_PER_HOUR = 20;
 		int empHours = 0;
 		int empWage = 0;
 		
 		double empCheck = Math.floor(Math.random() * 10) % 3;
 		
-		if (empCheck == IS_FULL_TIME) {
+		switch ((int)empCheck) { 
+		
+		case (IS_FULL_TIME):
 			System.out.println("Employee is Present");
 			empHours = 8;
 			empWage = empHours * EMP_RATE_PER_HOUR;
 			System.out.println("Employee Wage: " + empWage);
-		}	
-		else if(empCheck == IS_PART_TIME){
+			break;
+			
+		case (IS_PART_TIME):
 			System.out.println("Part Time Employee is Present");
 			empHours = 4;
 			empWage = empHours * EMP_RATE_PER_HOUR;
 			System.out.println("Employee Wage: " + empWage);
-		}
-		else {
+			break;
+			
+		default:
 			System.out.println("Employee is Absent");
 			System.out.println("Employee Wage: " + empWage);
 		}
