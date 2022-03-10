838571209c5b6d8b9c3b77d689232e28016c580d
Swapnil_Kamble <swapnilskamble010@gmail.com>
2022-03-10 17:01:11 +0530

    UC1 logic is added with welcome message

---
+++ EmpWageBuilder.java
@@ -0,0 +1,16 @@
+package empwage;
+
+public class EmpWageBuilder {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage Computation Program");
+		int IS_FULL_TIME = 1;
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		
+		if (empCheck == IS_FULL_TIME)
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Absent");
+	}
+
+}
