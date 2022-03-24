d7554f1fc8b644b9a6876b19e18d47d98496cf41
Swapnil_Kamble <swapnilskamble010@gmail.com>
2022-03-24 18:37:16 +0530

    Modify: Saving total wage for each company

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -4,8 +4,21 @@ public class EmpWageBuilder {
 	public static final int IS_FULL_TIME = 1; 
 	public static final int IS_PART_TIME = 2; 
 	
+
+	private final String company;
+	private final int empRatePerHr;
+	private final int numOfWorkingDays;
+	private final int maxHrsPerMonth;
+	private int totalEmpWage;
+	
 	public EmpWageBuilder(String company, int empRatePerHour, int numOfWorkingDays, int maxHrsPerMonth) {
-		int totalEmpWage = 0;
+		this.company = company;
+		this.empRatePerHr = empRatePerHour;
+		this.numOfWorkingDays = numOfWorkingDays;
+		this.maxHrsPerMonth = maxHrsPerMonth;
+	}
+	
+	public void empWageCalc() {
 		int totalEmpHour = 0;
 		int empHour = 0;
 		int totalWorkingDays = 0;
@@ -27,14 +40,24 @@ public class EmpWageBuilder {
 			}
 			totalEmpHour += empHour;
 		}
-		totalEmpWage = totalEmpHour * empRatePerHour;	
-		System.out.println("Total Employee Wage for Company " + company + " is : " + totalEmpWage);
+		totalEmpWage = totalEmpHour * empRatePerHr;	
 	}
 	
+	@Override
+	public String toString() {
+		return "[Total Employee Wage for Company " + company + " is: " + totalEmpWage + "]";
+	}
+
 	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage Computation Program using OOP's concepts");
-		EmpWageBuilder dMart = new EmpWageBuilder("DMart", 20, 30, 150);
+		System.out.println("Welcome to Employee Wage Computation");
+		EmpWageBuilder dMart = new EmpWageBuilder("D-Mart", 20, 30, 150);
+		dMart.empWageCalc();
+		System.out.println(dMart);
 		EmpWageBuilder bigBasket = new EmpWageBuilder("Big Basket", 25, 30, 200);
+		bigBasket.empWageCalc();
+		System.out.println(bigBasket);
 		EmpWageBuilder relianceMart = new EmpWageBuilder("Reliance Mart", 22, 30, 300);
+		relianceMart.empWageCalc();
+		System.out.println(relianceMart);
 	}
 }
